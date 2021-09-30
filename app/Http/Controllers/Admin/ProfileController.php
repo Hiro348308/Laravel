6916beba1d8cd7f71b52c807dac932be0cfdac53
hiroto.suka@rakuten-sec.co.jp <s-hiroto.suka@rakuten.com>
2021-09-30 15:53:13 +0900

    3_6

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -8,26 +8,31 @@ use App\Profile;
 
 class ProfileController extends Controller
 {
+public function add()
+{ 
+  return view('admin.profile.create');
+}
+public function edit()
+{
+    return view('admin.profile.edit');
+}
+public function create(Request $request)
+{
+
+  // 以下を追記
+  // Varidationをおこなう
+  $this->validate($request, Profile::$rules);
+
+  $profile = new Profile;
+  $form = $request->all();
+
+  // フォームから画像が送信されてきたら、保存して、$news->image_path に画像のパスを保存する
+  if (isset($form['image'])) {
+    $path = $request->file('image')->store('public/image');
+    $news->image_path = basename($path);
+  } else
   public function add()
-  {
-      return view('admin.profile.create');
-  }
-
-  public function create(Request $request)
-  {
-
-      // 以下を追記
-      // Varidationをおこなう
-      $this->validate($request, Profile::$rules);
-
-      $profile = new Profile;
-      $form = $request->all();
-
-      // フォームから画像が送信されてきたら、保存して、$news->image_path に画像のパスを保存する
-      if (isset($form['image'])) {
-        $path = $request->file('image')->store('public/image');
-        $news->image_path = basename($path);
-      } else {
+ {
           $profile->image_path = null;
       }
 
