0a48e6f3cd85e2e67144378cd159503b44923e45
hiroto.suka@rakuten-sec.co.jp <s-hiroto.suka@rakuten.com>
2021-09-30 18:36:53 +0900

    3_13

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -1,51 +1,83 @@
 <?php
+
 namespace App\Http\Controllers\Admin;
-use Illuminate\Http\Request;
+
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 // 以下を追記することでNews Modelが扱えるようになる
+
 use App\Profile;
+
+// 以下を追記
+use App\ProfileHistory;
+use Carbon\Carbon;
+use Symfony\Component\HttpKernel\Profiler\ProfilerStorageInterface;
 
 class ProfileController extends Controller
 {
-public function add()
-{ 
-  return view('admin.profile.create');
-}
-public function edit()
+    public function add()
 {
-    return view('admin.profile.edit');
+    return view('admin.profile.create');
+    
 }
+
 public function create(Request $request)
 {
+    $this->validate($request, Profile::$rules);
 
-  // 以下を追記
-  // Varidationをおこなう
-  $this->validate($request, Profile::$rules);
+    $profile = new Profile();
+    $form = $request->all();
+    unset($form['_token']);
 
-  $profile = new Profile;
-  $form = $request->all();
+    $profile->fill($form);
+    $profile->save();
+    
+    return redirect('admin/profile/create');
+}
 
-  // フォームから画像が送信されてきたら、保存して、$news->image_path に画像のパスを保存する
-  if (isset($form['image'])) {
-    $path = $request->file('image')->store('public/image');
-    $news->image_path = basename($path);
-  } else
-  public function add()
- {
-          $profile->image_path = null;
+public function edit(Request $request)
+{
+      // News Modelからデータを取得する
+      $profile = Profile::find($request->id);
+      if (empty($profile)) {
+        abort(404);    
       }
+    return view('admin.profile.edit', ['profile_form' => $profile]);
+}
 
-      // フォームから送信されてきた_tokenを削除する
-      unset($form['_token']);
-      // フォームから送信されてきたimageを削除する
-      unset($form['image']);
+public function update(Request $request)
+{
+     // Validationをかける
+     $this->validate($request, Profile::$rules);
+     // News Modelからデータを取得する
+     $profile = Profile::find($request->id);
+     // 送信されてきたフォームデータを格納する
+     $profile_form = $request->all();
+     unset($profile_form['_token']);
+     // 該当するデータを上書きして保存する
+     $profile->fill($profile_form)->save();
+     
+     $history = new ProfileHistory;
+     $history->profile_id = $profile->id;
+     $history->edited_at = Carbon::now();
+     $history->save();
 
-      // データベースに保存する
-      $profile->fill($form);
-      $profile->save();
+    return redirect('admin/profile');
+}
 
-      return redirect('admin/profile/create');
+ public function index(Request $request)
+  {
+      $cond_title = $request->cond_title;
+      if ($cond_title != '') {
+          // 検索されたら検索結果を取得する
+          $posts = Profile::where('title', $cond_title)->get();
+      } else {
+          // それ以外はすべてのニュースを取得する
+          $posts = Profile::all();
+      }
+      return view('admin.profile.index', ['posts' => $posts, 'cond_title' => $cond_title]);
   }
+
 
 }
